b4a2207027bd209f10f2dbcccbb582d8dcbfcbcf
Aakashkumar Singh <aakashsingh688@gmail.com>
2025-07-04 15:45:32 +0530

    note

--- index.js
+++ index.js
@@ -3,22 +3,50 @@ import moment from "moment";
 import simpleGit from "simple-git";
 import random from "random";
 
-const path = "./data.json";
+const simpleGit = require('simple-git');
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const fs = require('fs');
+const path = require('path');
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+// Configure paths
+const DATA_FILE = path.join(__dirname, 'data.json');
+const LOG_FILE = path.join(__dirname, 'commit_log.txt');
 
+// Initialize log file
+fs.writeFileSync(LOG_FILE, 'Commit Log:\n', { flag: 'w' });
+
+// Function to log messages to file and console
+function logMessage(message) {
+  const timestamp = new Date().toISOString();
+  const logEntry = `[${timestamp}] ${message}\n`;
+  console.log(logEntry.trim());
+  fs.appendFileSync(LOG_FILE, logEntry);
+}
+
+// Function to create a single commit
+const makeCommit = (date) => {
   const data = {
-    date: date,
+    date: date.format(),
   };
-
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  
+  return new Promise((resolve, reject) => {
+    jsonfile.writeFile(DATA_FILE, data, (err) => {
+      if (err) {
+        logMessage(`Error writing to file: ${err}`);
+        reject(err);
+      } else {
+        simpleGit().add([DATA_FILE]).commit(date.format(), { "--date": date.format() }).push((err, data) => {
+          if (err) {
+            logMessage(`Error committing: ${err}`);
+            reject(err);
+          } else {
+            logMessage(`Committed: ${date.format()}`);
+            resolve();
+          }
+        });
+      }
+    });
   });
 };
 
